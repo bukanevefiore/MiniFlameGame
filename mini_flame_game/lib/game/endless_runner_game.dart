281ea6f2af528c956b2baa244d78c3d434c3b7d0
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2026-08-06 14:18:44 +0300

    Create asset directory structure

--- mini_flame_game/lib/game/endless_runner_game.dart
+++ mini_flame_game/lib/game/endless_runner_game.dart
@@ -6,7 +6,6 @@ import 'world/game_world.dart';
 
 class EndlessRunnerGame extends FlameGame {
   late final CameraComponent cameraComponent;
-  late final GameWorld gameWorld;
 
   @override
   Color backgroundColor() => const Color(0xFF111111);
@@ -15,15 +14,14 @@ class EndlessRunnerGame extends FlameGame {
   Future<void> onLoad() async {
     await super.onLoad();
 
-    gameWorld = GameWorld();
+    world = GameWorld();
 
     cameraComponent = CameraComponent.withFixedResolution(
-      world: gameWorld,
+      world: world,
       width: 360,
       height: 640,
     );
 
-    await add(gameWorld);
-    await add(cameraComponent);
+    camera = cameraComponent;
   }
 }
