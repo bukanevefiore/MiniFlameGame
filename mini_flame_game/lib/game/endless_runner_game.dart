549bc3d24f23c6a1f51658c01fb5ad4cad189376
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2026-08-06 03:36:18 +0300

    Initialize Flame game with GameWidget

---
+++ mini_flame_game/lib/game/endless_runner_game.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+
+class EndlessRunnerGame extends FlameGame {
+  @override
+  Color backgroundColor() {
+    return const Color(0xFF111111);
+  }
+}
