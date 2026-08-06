c780264c758d8a3d19edb6e2a5e6b2289ef94ff6
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2026-08-06 04:04:02 +0300

    Add GameWorld and CameraComponent

--- mini_flame_game/lib/game/endless_runner_game.dart
+++ mini_flame_game/lib/game/endless_runner_game.dart
@@ -1,12 +1,29 @@
+import 'package:flame/camera.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'world/game_world.dart';
+
 class EndlessRunnerGame extends FlameGame {
+  late final CameraComponent cameraComponent;
+  late final GameWorld gameWorld;
+
   @override
   Color backgroundColor() => const Color(0xFF111111);
 
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+
+    gameWorld = GameWorld();
+
+    cameraComponent = CameraComponent.withFixedResolution(
+      world: gameWorld,
+      width: 360,
+      height: 640,
+    );
+
+    await add(gameWorld);
+    await add(cameraComponent);
   }
 }
