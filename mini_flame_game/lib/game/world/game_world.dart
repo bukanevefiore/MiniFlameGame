8dd0aa8b4e0dac7d3535d0af7206528e728d5f66
Melis Nur KILIÇ <melisnurklc0@gmail.com>
2026-08-07 07:54:52 +0300

    add player component

--- mini_flame_game/lib/game/world/game_world.dart
+++ mini_flame_game/lib/game/world/game_world.dart
@@ -1,8 +1,12 @@
 import 'package:flame/components.dart';
 
+import '../components/player_component.dart';
+
 class GameWorld extends World {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
+
+    add(PlayerComponent());
   }
 }
